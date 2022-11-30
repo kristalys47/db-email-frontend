e9a58804214c8049aa7bc7540ebbbccf497d6350
kristalys47 <kristalys.cba4@gmail.com>
2022-11-29 23:06:15 -0400

    Doneeee

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '../node_modules/semantic-ui-css/semantic.min.css'
+import HomePage from "./HomePage";
+import UserView from "./UserView";
+import Dashboard from "./Dashboard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot( document.getElementById('root') );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/Home" element={<HomePage/>} />
+            <Route exact path="/UserView" element={<UserView/>} />
+            <Route exact path="/Dashboard" element={<Dashboard/>} />
+        </Routes>
+    </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
